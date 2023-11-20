62acfee488d115c50cfed2a09b0d15c193b3e103
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-11-19 23:47:17 -0600

    continued jframe exercise

--- src/main/java/com/example/java2dgame/Game.java
+++ src/main/java/com/example/java2dgame/Game.java
@@ -1,20 +1,28 @@
 package com.example.java2dgame;
+
 import javax.swing.JFrame;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
 
 public class Game extends Canvas implements Runnable {
 
     private static final long serialVersionUID = 1L;
 
     public static final int WIDTH = 160;
-    public static final int HEIGHT = WIDTH /12*9;
+    public static final int HEIGHT = WIDTH / 12 * 9;
     public static final int SCALE = 3;
     public static final String NAME = "GAME";
 
     private JFrame frame;
 
     public boolean running = false;
+    public int tickCount = 0;
 
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
     public Game() {
         setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
         setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -25,7 +33,7 @@ public class Game extends Canvas implements Runnable {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        frame.add(this,BorderLayout.CENTER);
+        frame.add(this, BorderLayout.CENTER);
         frame.pack();
 
         frame.setResizable(false);
@@ -33,19 +41,84 @@ public class Game extends Canvas implements Runnable {
         frame.setVisible(true);
 
     }
+
     public synchronized void start() {
         running = true;
         new Thread(this).start();
     }
 
     public synchronized void stop() {
-    running = false;
+        running = false;
     }
+
     @Override
     public void run() {
-    while(running) {
-        System.out.println("Hello World");
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000D / 60D;
+
+        int frames = 0;
+        int ticks = 0;
+
+        long lasTimer = System.currentTimeMillis();
+        double delta = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / nsPerTick;
+            lastTime = now;
+            boolean shouldRender = true;
+
+            while (delta >= 1) {
+                ticks++;
+                tick();
+                delta -= 1;
+                shouldRender = true;
+            }
+
+            try {
+                Thread.sleep(2);
+            }catch(InterruptedException e) {
+                e.printStackTrace();;
+            }
+            if(shouldRender) {
+                frames++;
+                render();
+            }
+
+            if(System.currentTimeMillis() - lasTimer >= 1000) {
+                lasTimer += 1000;
+                System.out.println(frames + " frames" + "," + ticks + " ticks");
+                frames = 0;
+                ticks = 0;
+            }
+        }
     }
+
+    public void tick() {
+    tickCount++;
+
+    for(int i =0; i < pixels.length; i++){
+        pixels[i] = i + tickCount;
+        }
+
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if(bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+//        g.setColor(Color.BLACK);
+//
+//        g.fillRect(0,0, getWidth(), getHeight());
+
+        g.drawImage(image, 0,0, getWidth(), getHeight(), null);
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
